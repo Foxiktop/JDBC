17b9a2dd8979818bb359d26d42fa7735068ebe6d
TaniaSorokina <tania_sorokina@mail.ru>
2024-01-09 20:21:52 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,5 +3,27 @@ package jm.task.core.jdbc;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 44);
+        userService.saveUser("Егор", "Егоров", (byte) 25);
+        userService.saveUser("Петр", "Петров", (byte) 37);
+        userService.saveUser("Борис", "Борисов", (byte) 20);
+
+        List<User> listUsers = userService.getAllUsers();
+        for (User user : listUsers) {
+            System.out.println(user.toString());
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
     }
 }
+
+
+
