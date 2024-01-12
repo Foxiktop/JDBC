b97c638ff0b0fb3772b2a569243911133277118b
TaniaSorokina <tania_sorokina@mail.ru>
2024-01-12 19:58:16 +0300

    Решение задачи jdbs

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // реализуйте алгоритм здесь
 
-        UserService userService = new UserServiceImpl();
+       UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
 
@@ -18,7 +23,7 @@ public class Main {
             System.out.println(user.toString());
         }
 
-        userService.cleanUsersTable();
+       userService.cleanUsersTable();
 
         userService.dropUsersTable();
 
